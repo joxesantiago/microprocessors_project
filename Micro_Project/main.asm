a5d4fa6771730e204ec52d52bf450e2b81a0a39d
alejandra <alejandra.casanova@upr.edu>
2018-11-10 22:50:02 -0400

    Saves 2 caracters per register... Needs to be more efficient.

--- Micro_Project/main.asm
+++ Micro_Project/main.asm
@@ -16,6 +16,10 @@
                                             ; references to current section.
 	.sect ".sysmem"
 x .byte "+500, +200, +"						;Saves the string in x
+sum .byte "+"
+rest .byte "-"
+div .byte "/"
+mult .byte "*"
 
 ;-------------------------------------------------------------------------------
 RESET       mov.w   #__STACK_END,SP         ; Initialize stackpointer
@@ -24,7 +28,25 @@ StopWDT     mov.w   #WDTPW|WDTHOLD,&WDTCTL  ; Stop watchdog timer
 
 ;-------------------------------------------------------------------------------
 ; Main loop here
-			mov #x,R7
+			mov #x,R7						;saves the pointer to the string to R7
+			mov.b sum,
+			mov 0(R7), R7
+			mov 2(R7), R8					;Se guarda
+			mov 4(R7), R9
+			mov 6(R7), R10
+			mov 8(R7), R11
+			mov 10(R7), R12
+			mov 12(R7), R13
+			mov 14(R7), R14
+
+
+
+
+;Ale lo q quieres es leer los signos dentro de ese string, los tres.
+; Una opcion para no usar tanta memoria es crear if statements. Si todos son + pues haces la subrutina de suma relax
+; Si hay uno negativo dentro de los primeros dos pues haces el 2's complement...
+; sigue pensando esto ma~ana... tienes sue~o
+
 ;--------------------------------------------------------------------------------
 
 ;-------------------------------------------------------------------------------
